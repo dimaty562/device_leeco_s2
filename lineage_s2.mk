707575f1b3bce544ad21207a8bac1e3b21aa61d2
dimaty562 <nikiforovd17@gmail.com>
2020-08-01 12:16:04 +0000

    s2: LineageOS -> Halium

--- lineage_s2.mk
+++ lineage_s2.mk
@@ -16,13 +16,15 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
 
 # Inherit from s2 device
 $(call inherit-product, device/leeco/s2/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+#$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := LeEco
